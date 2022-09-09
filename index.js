247cc79286fa42edcdfc3bd41226f1d2937018c7
Vitor Daniel <106852114+VitorDanielA@users.noreply.github.com>
2022-09-09 16:25:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,29 +22,3 @@ function iphoneBlue(){
     var img = document.getElementById("img");
     img.src = 'images/iphone_blue.jpg'
 }
-
-const buttons = document.querySelectorAll("#model-cores li");
-const image = document.querySelector("#img");
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    console.log(e);
-
-    buttons.forEach((btn) =>
-      btn.querySelector(".color").classList.remove("selected")
-    );
-
-    const button = e.target;
-
-    const id = button.getAttribute("id");
-
-    button.querySelector(".verde").classList.add("selected");
-
-    image.classList.toggle("changing");
-    image.setAttribute("src", `img/iphone_${id}.jpg`);
-
-    setTimeout(() => {
-      image.classList.toggle("changing");
-    }, 200);
-  });
-});
